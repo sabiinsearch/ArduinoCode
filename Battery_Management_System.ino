f4709f5658146bd1fabaa40131de6016addaa755
sabiinsearch <sabiinsearch@gmail.com>
2024-08-04 12:20:30 +0530

    update

---
+++ Battery_Management_System.ino
@@ -0,0 +1,55 @@
+#include <Wire.h>
+
+// my libararies
+#include "receiverBoard.h"
+#include "addresses.h"
+#include "appManager.h"
+
+
+appManager manager;
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+  Serial.begin(9600);
+  
+  appManager_ctor(&manager);
+
+  initBoard();
+  
+}
+
+void loop() {
+
+     blink();       
+
+     scan(&manager);
+
+        if(!(manager.connected_bat)) {
+          
+            resetCharger();         
+        }      
+        else {
+              
+              if(manager.relative_soc<100) {
+               
+               if(digitalRead(CHARGE_FULL_LED) == HIGH) {
+                  digitalWrite(CHARGE_FULL_LED,HIGH);
+               }
+                setCharger(&manager);  
+               // digitalWrite(CHARGING_LED_ON,LOW);     
+              }else {
+               digitalWrite(CHARGE_FULL_LED,LOW);
+               resetCharger();
+               digitalWrite(CHARGING_LED_ON,HIGH);
+              }
+                           
+                 printBatInfo(&manager); 
+         }                    
+
+          blink();
+
+   delay(1000);
+  }
+
+
--- main/myCust_libs/scanner.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SCANNER_H__
-#define __SCANNER_H__
-
-
-
-#endif
